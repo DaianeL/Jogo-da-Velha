96048dcbbb24f2cc9abd6219ad0e904ce9c360bc
Daiane Lima <lima.dai@outlook.com>
2023-03-29 08:11:47 -0300

    Atualização de código 29/03/2023

--- script.js
+++ script.js
@@ -1,1 +1,11 @@
-console.log("teste");
+let x = document.querySelector(".x");
+let o = document.querySelector(".o");
+let boxes = document.querySelectorAll(".box");
+let buttons = document.querySelectorAll("#buttons-container button");
+let messageContainer = document.querySelector("#message");
+let messageText = document.querySelector("#message p");
+let secondPlayer;
+
+//Criando um contador de jogadas
+let player1 = 0;
+letplayer2 = 0;
